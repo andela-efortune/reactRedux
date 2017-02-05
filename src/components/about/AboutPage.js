190bdbb85940b5c5feaf141f6b20e0f565379266
Fortune Ekeruo <legalmody@gmail.com>
2017-02-05 21:13:00 +0100

    App template layout used for rendering all other pages

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -5,7 +5,10 @@ class AboutPage extends React.Component {
     return (
       <div>
         <h1>About</h1>
-        <p>This application uses React, Redux, React router and a variety of other helpful libaries.</p>
+        <p>
+          This application uses React, Redux, React router
+          and a variety of other helpful libaries.
+        </p>
       </div>
     );
   }
