89a2b410e4ce3c4fdfa3e5084434e3dd505ff820
Fortune Ekeruo <legalmody@gmail.com>
2017-02-05 22:31:22 +0100

    Add a common component Header.js for our navigation bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 // This Component handles the App template used on every page
 import React, {PropTypes} from 'react';
-
+import Header from './common/Header';
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <p>Header...</p>
+        <Header />
         {this.props.children}
       </div>
     );
