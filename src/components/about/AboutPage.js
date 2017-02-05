4a3da6f31fd38881bbc7cc19010382039bd3e17e
Fortune Ekeruo <legalmody@gmail.com>
2017-02-05 19:50:56 +0100

    Add an AboutPage component

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This application uses React, Redux, React router and a variety of other helpful libaries.</p>
+      </div>
+    );
+  }
+}
