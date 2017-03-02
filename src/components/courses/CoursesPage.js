1122c4b7df2d66b2a8364bb9bbf791e74c438b6d
Fortune Ekeruo <legalmody@gmail.com>
2017-03-02 11:32:39 +0100

    Add a constructor for the initial state of the form to be created

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,13 +1,23 @@
 import React, {PropTypes} from 'react';
 
 class CoursesPage extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Courses</h1>
-      </div>
-    );
-  }
+	constructor(props, context) {
+		super(props, context);
+
+		this.state = {
+			course: {
+				title: null
+			}
+		};
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Courses</h1>
+			</div>
+		);
+	}
 }
 
 export default CoursesPage;
