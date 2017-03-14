138d1153373df71cb7b39cc76b44cd68848d554f
Fortune Ekeruo <legalmody@gmail.com>
2017-03-14 12:45:52 +0100

    Use thunk for making an async call and create a load courses thunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,8 +1,16 @@
 import * as types from './actionTypes';
+import courseApi from './../api/mockCourseApi';
 
-export function createCourse(course) {
-	return {
-		type: types.CREATE_COURSE,
-		course
+export function loadCoursesSuccess(courses) {
+	return { type: types.LOAD_COURSE_SUCCESS, courses};
+}
+
+export function loadCourses() {
+	return function(dispatch) {
+		courseApi.getAllCourses().then(courses => {
+			dispatch(loadCoursesSuccess(courses));
+		}).catch(error => {
+			throw error;
+		});
 	};
 }
